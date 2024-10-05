91ecfd88baf1f38ebb032c45ec00bd7e3854230c
Alexander Egger <s2210438004@fhooe.at>
2024-10-05 16:57:35 +0200

    implemented myEnums

---
+++ include/myEnums.h
@@ -0,0 +1,14 @@
+#ifndef MYENUMS_H
+#define MYENUMS_H
+
+// Definition des Board-Enums
+enum Board
+{
+    Magazin = 8,
+    SchereUndSchlitten,
+    Roboter,
+    TransportUndWechsler,
+    NumberOfBoards
+};
+
+#endif // MYENUMS_H
