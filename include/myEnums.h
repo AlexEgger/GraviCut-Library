518c468885e07d97265d7d5a3f16940b18239928
Alexander Egger <s2210438004@fhooe.at>
2024-10-12 10:30:26 +0200

    now library only contains Enums

---
+++ include/myEnums.h
@@ -0,0 +1,14 @@
+#ifndef MYENUMS_H
+#define MYENUMS_H
+
+// Enumeration for modules
+enum class Module {
+    Magazin = 0,
+    Schieber,
+    Schere,
+    Wechsler,
+    Anschlag,
+    NumberOfModules
+};
+
+#endif // MYENUMS_H
